d2a102763a136029ded7d5ca7df1e9c172b1f78e
Adam Morsi <adam.morsi@yahoo.com>
2021-10-16 12:38:17 +0300

    add main UI of the city overview component

--- src/js/containers/pages/WeatherPage.jsx
+++ src/js/containers/pages/WeatherPage.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
+//styles
+import styles from './WeatherPage.scss';
+//custom hooks
+import useBoolean from '../../customHooks/UseBoolean';
 //components
 import CityWeatherDetails from '../../components/cityWeatherDetails/CityWeatherDetails';
 import CityOverview from '../../components/cityOverView/CityOverview';
 
 const WeatherPage = () => {
 	const dispatch = useDispatch(),
-		[isDetails, setIsDetails] = useState(false);
+		[isDetails, setIsDetails] = useBoolean(false);
 
 	return (
 		<div className="container" style={{ textAlign: 'center' }}>
-			{isDetails ? <CityWeatherDetails /> : <CityOverview />}
+			{isDetails ? (
+				<CityWeatherDetails />
+			) : (
+				<CityOverview city="Paris, FR" degree={25} status="Rain" />
+			)}
+			<span className={styles.seeMore} onClick={setIsDetails.toggle}>
+				{isDetails ? 'Hide' : 'Show'} details
+			</span>
 		</div>
 	);
 };
